87b8b379e7f4a67fc972e2d2f8e8c9c15f2246f1
CrashLearner <yonayen@hotmail.com>
2015-06-22 11:53:18 -0700

    Blue Color label tag

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -14,6 +14,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var enterNameTextField: UITextField!
     @IBOutlet weak var enterMessageTextField: UITextField!
     @IBOutlet weak var mailButton: UIButton!
+    
+    
+    @IBOutlet weak var nameLabel: UILabel!
         
     
     override func viewDidLoad() {
@@ -38,6 +41,16 @@ class ViewController: UIViewController {
         enterMessageTextField.resignFirstResponder()
         
        mailButton.setTitle("Mail Sent", forState: UIControlState.Normal)
+        
+        
+        nameLabel.hidden = false
+        nameLabel.text = enterNameTextField.text
+        nameLabel.textColor = UIColor.blueColor()
+        
+        enterNameTextField.text = ""
+        enterNameTextField.resignFirstResponder()
+        
+        
         
     }
     
